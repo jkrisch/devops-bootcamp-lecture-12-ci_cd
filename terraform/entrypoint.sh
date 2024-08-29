1247eb9d7f9f7b47892603e8ddf0db0d7c631fef
jonas <jonas.krisch@gmail.com>
2024-08-29 15:38:42 +0200

    adjust entrypoint script as docker-compose was not installed properly

--- terraform/entrypoint.sh
+++ terraform/entrypoint.sh
@@ -4,8 +4,5 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 #install docker-compose
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
